3d1a92eb31c1ad5cb97c13905bd9e1d61773136a
seanthompsonsnhu <sean.thompson2@snhu.edu>
2025-06-08 21:45:07 -0800

    Added procedural terrain

--- Source/firstperson415/firstperson415Projectile.cpp
+++ Source/firstperson415/firstperson415Projectile.cpp
@@ -8,6 +8,7 @@
 #include "Kismet/GameplayStatics.h"
 #include "NiagaraFunctionLibrary.h"
 #include "NiagaraComponent.h"
+#include "PerlinProcTerrain.h"
 
 Afirstperson415Projectile::Afirstperson415Projectile() 
 {
@@ -82,5 +83,12 @@ void Afirstperson415Projectile::OnHit(UPrimitiveComponent* HitComp, AActor* Othe
 
 		MatInstance->SetVectorParameterValue("Color", randColor);
 		MatInstance->SetScalarParameterValue("Frame", frameNum);
+
+		APerlinProcTerrain* procTerrain = Cast<APerlinProcTerrain>(OtherActor);
+
+		if (procTerrain)
+		{
+			procTerrain->AlterMesh(Hit.ImpactPoint);
+		}
 	}
 }
